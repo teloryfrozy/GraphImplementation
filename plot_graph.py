53a5c68d8fba2fcdb6d0e6daf1fe62ee46c915fa
teloryfrozy <raptor.frozy@gmail.com>
2023-12-19 10:45:11 +0100

    Add a plot graph for NASDA100

--- plot_graph.py
+++ plot_graph.py
@@ -6,6 +6,7 @@ from io import StringIO
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
+from tqdm import tqdm
 
 
 
@@ -36,22 +37,26 @@ else:
     print("No table found with id='constituents'.")
 
 
+frames, edgelist = [], []
+# creation of a loading bar
+#with tqdm(total=len(symbols)-1,  unit="%", bar_format='{percentage:3.0f}%|{bar}|') as pbar:
+for symbol in symbols:
+        var = yf.Ticker(symbol)
+        print(symbol)
+        # top 10 share holders
+        frame = var.institutional_holders
 
-frames = []
-
-for i, symbol in enumerate(symbols):
-    var = yf.Ticker(symbol)
-    frame = var.institutional_holders
-    print(i, symbol, frame)
-    frame['Company'] = var.ticker
-    
-    frames.append(frame)
-
-all_together = pd.concat(frames)
-print(all_together)
+        edgelist.append(frame.iloc[0]['Value']/50_000_000_000)
+        frame['Company'] = var.ticker
+        frames.append(frame)
+        # update progress bar for each symbol
+        #pbar.update(1)
 
 
+all_together = pd.concat(frames)#[:30]
+print(all_together, len(all_together))
 
+print(edgelist)
 
 G = nx.from_pandas_edgelist(all_together, 'Holder', 'Company')
 
@@ -65,22 +70,19 @@ for node in G:
 
 
 
-
-
-plt.figure(figsize=(50, 40))
-
     
 
 
-nx.draw(G, with_labels=True,
+#width=[v[2]['Value']/50_000_000_000 for v in edgelist]
+
+nx.draw_spring(G,
+        with_labels=True,
         node_color=colors,
-        node_size=[v * 100 for v in dict(G.degree()).values()],
-        
+        node_size=[v * 100 for v in dict(G.degree()).values()],     
+        width=edgelist,
+        edgecolors='black',
+        linewidths=1.5   
         )
 
-
-edgelist = nx.to_edgelist(G)
-print(edgelist)
-
-# width=[v[2]['Value']/50_000_000_000 for v in edgelist]
+plt.get_current_fig_manager().full_screen_toggle()
 plt.show()
